98265674c7d86bf3f948d2070879123956cf8bc8
Trent Hornibrook <trent.hornibrook@bjss.com>
2024-11-06 12:33:14 +1100

    updated

--- modules/admin-groups/main.tf
+++ modules/admin-groups/main.tf
@@ -8,40 +8,35 @@ data "azurerm_management_group" "data_platform" {
   name = var.data-platform-management-group-name
 }
 
-# Data source to fetch existing Azure AD groups
-data "azuread_group" "groups" {
-  for_each = toset([
-    "Azure Tenant Admin",
-    "Azure Tenant ReadOnly",
-    "Azure Data Platform Admin", 
-    "Azure Data Platform ReadOnly"
-  ])
-  
-  display_name = each.key
+locals {
+  group_ids = {
+    "Azure Tenant Admin"           = "121a028c-4a65-4751-95c5-3cfe873ba2b4"
+    "Azure Tenant ReadOnly"        = "121a028c-4a65-4751-95c5-3cfe873ba2b4"
+    "Azure Data Platform Admin"    = "121a028c-4a65-4751-95c5-3cfe873ba2b4"
+    "Azure Data Platform ReadOnly" = "121a028c-4a65-4751-95c5-3cfe873ba2b4"
+  }
 }
 
-# Role Assignments for Tenant Landing Zone Management Group
 resource "azurerm_role_assignment" "lz_tenant_admin_role" {
   scope                = data.azurerm_management_group.lz.id
   role_definition_name = "Owner"
-  principal_id         = data.azuread_group.groups["Azure Tenant Admin"].id
+  principal_id         = local.group_ids["Azure Tenant Admin"]
 }
 
 resource "azurerm_role_assignment" "lz_tenant_readonly_role" {
   scope                = data.azurerm_management_group.lz.id
   role_definition_name = "Reader"
-  principal_id         = data.azuread_group.groups["Azure Tenant ReadOnly"].id
+  principal_id         = local.group_ids["Azure Tenant ReadOnly"]
 }
 
-# Role Assignments for Data Platform Management Group
 resource "azurerm_role_assignment" "data_platform_admin_role" {
   scope                = data.azurerm_management_group.data_platform.id
   role_definition_name = "Owner"
-  principal_id         = data.azuread_group.groups["Azure Data Platform Admin"].id
+  principal_id         = local.group_ids["Azure Data Platform Admin"]
 }
 
 resource "azurerm_role_assignment" "data_platform_readonly_role" {
   scope                = data.azurerm_management_group.data_platform.id
   role_definition_name = "Reader"
-  principal_id         = data.azuread_group.groups["Azure Data Platform ReadOnly"].id
+  principal_id         = local.group_ids["Azure Data Platform ReadOnly"]
 }
