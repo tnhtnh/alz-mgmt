4261eb43b64761c27d0d67f47b7dec9c7cecdfe8
Trent Hornibrook <trent.hornibrook@bjss.com>
2024-11-06 12:26:14 +1100

    fixing bug

--- modules/admin-groups/main.tf
+++ modules/admin-groups/main.tf
@@ -22,14 +22,12 @@ data "azuread_group" "groups" {
 
 # Role Assignments for Tenant Landing Zone Management Group
 resource "azurerm_role_assignment" "lz_tenant_admin_role" {
-  name                 = "lz-tenant-admin-owner-role"
   scope                = data.azurerm_management_group.lz.id
   role_definition_name = "Owner"
   principal_id         = data.azuread_group.groups["Azure Tenant Admin"].id
 }
 
 resource "azurerm_role_assignment" "lz_tenant_readonly_role" {
-  name                 = "lz-tenant-readonly-reader-role"
   scope                = data.azurerm_management_group.lz.id
   role_definition_name = "Reader"
   principal_id         = data.azuread_group.groups["Azure Tenant ReadOnly"].id
@@ -37,14 +35,12 @@ resource "azurerm_role_assignment" "lz_tenant_readonly_role" {
 
 # Role Assignments for Data Platform Management Group
 resource "azurerm_role_assignment" "data_platform_admin_role" {
-  name                 = "data-platform-admin-owner-role"
   scope                = data.azurerm_management_group.data_platform.id
   role_definition_name = "Owner"
   principal_id         = data.azuread_group.groups["Azure Data Platform Admin"].id
 }
 
 resource "azurerm_role_assignment" "data_platform_readonly_role" {
-  name                 = "data-platform-readonly-reader-role"
   scope                = data.azurerm_management_group.data_platform.id
   role_definition_name = "Reader"
   principal_id         = data.azuread_group.groups["Azure Data Platform ReadOnly"].id
